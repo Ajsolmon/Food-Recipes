c30b7cd75b8d13f191515ec8e0e94324a8b61f20
Ajsolmon <Ajsolmon@users.noreply.github.com>
2020-08-15 21:36:39 +0100

    files attached

---
+++ FoodDB/index.js
@@ -0,0 +1,415 @@
+const recipes = `[
+{	
+    "name"		: "Jollof Rice",
+    "image": "https://i.ibb.co/0jf5K8s/Jollof-Rice.jpg",
+    
+    "ingredient" : [
+    {"ingredients": "5 medium sized Roma tomatoes, roughly chopped"},
+    {"ingredients": "1 red bell pepper, roughly chopped"},
+    {"ingredients": "1 medium sized onion, roughly chopped, set aside"},
+    {"ingredients": "2 scotch bonnet peppers (habanero peppers as they are sometimes called)"},
+    {"ingredients": "¼ cup of groundnut oil"},
+    {"ingredients": "3 tbsp tomato paste"},
+    {"ingredients": "2 cups of parboiled rice"},
+    {"ingredients": "2½ cups of chicken stock"},
+    {"ingredients": "1 tsp salt to taste"},
+    {"ingredients": "½ tsp curry powder"},
+    {"ingredients": "½ tsp thyme"},
+    {"ingredients": "1 tsp All purpose seasoning"},
+    {"ingredients": "1 Knorr stock cube"},
+    {"ingredients": "3 bay leaves"},
+    {"ingredients": "Water, as needed"}
+    ],
+    
+    "instruction" : [
+    {"instructions": "Blend your tomatoes, red pepper, scotch bonnet peppers in a food processor or blender for about 45 seconds, make sure that everything is blended well."},
+    {"instructions": "In a medium sized pot, heat your oil on medium-high heat. Once the oil is heated add the onions you set aside and fry just until they turn golden brown. Once the onions, have turned brown in color add the tomato paste and fry for 2-3 minutes. Then add the blended tomato mixture (reserve about ¼ cup and set aside) and fry the mixture with the onions and tomato paste for about 30 minutes. Make sure you stir consistently so that the tomato mixture does not burn."},
+    {"instructions": "After 30 minutes, turn the heat down to medium, and add the chicken stock. Mix and add your seasonings (salt, curry powder, thyme, all purpose seasoning, and the Knorr stock cube). Continue to boil for 10 minutes."},
+    {"instructions": "Add the parboiled rice** to the pot. Mix it very well with the tomato stew. At this point if you need to add water so that the rice is level with the tomato mixture/chicken stock go ahead and do so. Add the bay leaves, cover the pot, and cook on medium to low heat for 15-30 minutes."},
+    {"instructions": "When the liquid has almost dried up add the remaining tomato stew**, cover, and let it cook for another 5-10 minutes heat until the liquid has completely dried up. Turn off the heat, mix thoroughly, and your Jollof Rice is ready to be eaten!"}
+    ]
+},
+{
+    "name"		: "Yam and Egg Sauce",
+    "image": "https://i.ibb.co/VxbBHyF/egg-sauce-yam.png",
+    
+    "ingredient" : [
+    {"ingredients": "1/2 Tuber Yam"},
+    {"ingredients": "7 eggs"},
+    {"ingredients": "3 fresh tomatoes diced"},
+    {"ingredients": "4 fresh pepper diced"},
+    {"ingredients": "1 onions diced"},
+    {"ingredients": "1 cksp veg oil"},
+    {"ingredients": "to taste salt"},
+    {"ingredients": "1 maggi cube"},
+    {"ingredients": "1 green bell pepper diced"}
+    ],
+
+    "instruction" : [
+    {"instructions": "Peel and wash yam and per boil with dash of salt once its soft and tender turn off heat, drain and turn off heat."},
+    {"instructions": "Break eggs in a bowl and add apinch of salt and whisk properly; heat up frying pan and once it hot add veg oil diced onions and stir in tomatoes, salt, maggi and pepper fry for 7mins"},
+    {"instructions": "Pour egg into the frying pan and scramble everything together, finally add the green bell pepper and scramble everything together. turn off heat immediately and serve with boiled yam"}
+    ]
+},
+{
+    "name": "Doughnuts",
+    "image": "https://i.ibb.co/PjT26T5/Doughnuts.jpg",
+    
+    "ingredient" : [
+    {"ingredients": "6 cups flour"},
+    {"ingredients": "1 cup whole milk"},
+    {"ingredients": "1 cup sugar"},
+    {"ingredients": "1.1 /2 tbsp dry yeast"},
+    {"ingredients": "3 large eggs"},
+    {"ingredients": "1/2 tsp salt"},
+    {"ingredients": "1/2 cup butter"}
+    ],
+        
+    "instruction" : [
+    {"instructions": "Add sugar and yeast to warm milk in a large bowl, set aside for 5 mins"},
+    {"instructions": "In a separate bowl add the flour, salt and butter mix together"},
+    {"instructions": "Add the eggs as well as the wet ingredients to form a dough"},
+    {"instructions": "Transfer it to a work surface and kneed the dough for 5-7 mins. Feel free to dust the surface with flour."},
+    {"instructions": "Roll out the dough and cut into doughnut shape"},
+    {"instructions": "Allow it to rise for 5mins"},
+    {"instructions": "Preheat oil and deep fry until golden brown"}
+    ]
+},
+
+{
+    "name"		: "Amala and Ewedu",
+    "image": "https://i.ibb.co/txvMZsc/Amala-and-Ewedu.jpg",
+    
+    "ingredient" : [
+    {"ingredients": "Amala powder"},
+    {"ingredients": "Salt"},
+    {"ingredients": "Ewedu"},
+    {"ingredients": "Salt"},
+    {"ingredients": "Chicken"},
+    {"ingredients": "Red pepper"},
+    {"ingredients": "Crayfish"},
+    {"ingredients": "Red oil"},
+    {"ingredients": "cubes Stock"},
+    {"ingredients": "Boiled chicken"}
+    ],
+
+    "instruction" : [
+    {"instructions": "Wash and blend the ewedu pour out and keep.heat up red oil fry the red pepper,salt,stock cubes,crayfish and the boiled chicken."},
+    {"instructions": "Pour in the blended ewedu boil small and bring it down."},
+    {"instructions": "Boil water pour in the amala powder stir and stir very well.cook add water to allow it cook well make sure no lump is in it by stirring very well."}
+    ]
+},
+
+{
+    "name"		: "Iyan and Egusi Soup",
+    "image": "https://i.ibb.co/HtjMGsM/Iyan-and-Egusi-soup.jpg",
+    
+    "ingredient" : [
+    {"ingredients": "Egusi"},
+    {"ingredients": "Boiled meat"},
+    {"ingredients": "3 medium dry fish"},
+    {"ingredients": "Stock fish"},
+    {"ingredients": "Grinded Cray fish"},
+    {"ingredients": "Seasoning"},
+    {"ingredients": "Uguleaf"},
+    {"ingredients": "Palm oil"},
+    {"ingredients": "Pepper and onions"},
+    {"ingredients": "Salt"},
+    {"ingredients": "Locust bean"}
+    ],
+
+    "instruction" : [
+    {"instructions": "In a pot,put 4 tablespoon of Palm oil with onions and fry"},
+    {"instructions": "Add blended pepper and onions and fry a bit,then add your locust beans and fry for few minutes"},
+    {"instructions": "Egusi soup with pounded yam recipe"},
+    {"instructions": "Then add your stock.(if it's not up to the quantity needed,you can add water)"},
+    {"instructions": "Then put in your boiled meat,stock fish,and dry fish"},
+    {"instructions": "Egusi soup with pounded yam recipe"},
+    {"instructions": "Add seasoning and salt to taste then cover your pot and let it cook for 10 to 20 mins"},
+    {"instructions": "When it boils, you can then put in your grinded egusi and Crayfish and allow it to cook well"},
+    {"instructions": "Wash and drain your ugu leaf"},
+    {"instructions": "Egusi soup with pounded yam recipe."},
+    {"instructions": "Then turn in the leaf in the boiling egusi pot for a minute then serve."}
+    ]
+},
+
+{
+    "name"		: "Fruit Salad",
+    "image": "https://i.ibb.co/NWQB0Kt/Fruit-Salad.jpg",
+
+    "ingredient" : [
+    {"ingredients": "Watermelon"},
+    {"ingredients": "Banana"},
+    {"ingredients": "Mango"},
+    {"ingredients": "Green apple"},
+    {"ingredients": "Red apples"},
+    {"ingredients": "Grapes"},
+    {"ingredients": "Fanta"}
+    ],
+
+    "instruction" : [
+    {"instructions": "Peel and cut all your fruits and put in a salad bowl then pour fanta to them.....you can add ice or refrigerate"}            
+    ]
+}
+]`
+
+
+
+const jollof = () =>{
+    const foodImage = document.getElementById('foodImage')
+    const foodName = document.getElementById('foodName')
+
+    const ingParent = document.getElementById('ingredientList')
+    const ingArr = JSON.parse(recipes)[0].ingredient
+    const instArr = JSON.parse(recipes)[0].instruction
+    const instParent = document.getElementById('instructionList')
+    const instList = document.createElement('li')
+
+    while(ingParent.hasChildNodes()){
+        ingParent.removeChild(ingParent.firstChild)
+    }
+    while(instParent.hasChildNodes()){
+        instParent.removeChild(instParent.firstChild)
+    }
+
+    function ing (all){
+        const ingList = document.createElement('li')
+        const ingListText = document.createTextNode(all)
+        
+        ingList.appendChild(ingListText)
+        return ingList
+    }
+    function inst (all){
+        const instList = document.createElement('li')
+        const instListText = document.createTextNode(all)
+        
+        instList.appendChild(instListText)
+        return instList
+    }
+
+    for(let i=0; i<ingArr.length; i++) {
+        ingParent.appendChild(ing(ingArr[i].ingredients))
+    }
+    for(let i=0; i<instArr.length; i++) {
+        instParent.appendChild(ing(instArr[i].instructions))
+    }
+    
+
+    foodImage.src = JSON.parse(recipes)[0].image
+    foodName.innerHTML = JSON.parse(recipes)[0].name
+}
+
+const amala = () =>{
+    const foodImage = document.getElementById('foodImage')
+    const foodName = document.getElementById('foodName')
+
+    const ingParent = document.getElementById('ingredientList')
+    const ingArr = JSON.parse(recipes)[3].ingredient
+    const instArr = JSON.parse(recipes)[3].instruction
+    const instParent = document.getElementById('instructionList')
+    const instList = document.createElement('li')
+
+    while(ingParent.hasChildNodes()){
+        ingParent.removeChild(ingParent.firstChild)
+    }
+    while(instParent.hasChildNodes()){
+        instParent.removeChild(instParent.firstChild)
+    }
+
+    function ing (all){
+        const ingList = document.createElement('li')
+        const ingListText = document.createTextNode(all)
+        
+        ingList.appendChild(ingListText)
+        return ingList
+    }
+    function inst (all){
+        const instList = document.createElement('li')
+        const instListText = document.createTextNode(all)
+        
+        instList.appendChild(instListText)
+        return instList
+    }
+
+    for(let i=0; i<ingArr.length; i++) {
+        ingParent.appendChild(ing(ingArr[i].ingredients))
+    }
+    for(let i=0; i<instArr.length; i++) {
+        instParent.appendChild(ing(instArr[i].instructions))
+    }
+    
+    foodImage.src = JSON.parse(recipes)[3].image
+    foodName.innerHTML = JSON.parse(recipes)[3].name
+}
+
+const doughnut = () =>{
+    const foodImage = document.getElementById('foodImage')
+    const foodName = document.getElementById('foodName')
+
+    const ingParent = document.getElementById('ingredientList')
+    const ingArr = JSON.parse(recipes)[2].ingredient
+    const instArr = JSON.parse(recipes)[2].instruction
+    const instParent = document.getElementById('instructionList')
+    const instList = document.createElement('li')
+
+    while(ingParent.hasChildNodes()){
+        ingParent.removeChild(ingParent.firstChild)
+    }
+    while(instParent.hasChildNodes()){
+        instParent.removeChild(instParent.firstChild)
+    }
+
+    function ing (all){
+        const ingList = document.createElement('li')
+        const ingListText = document.createTextNode(all)
+        
+        ingList.appendChild(ingListText)
+        return ingList
+    }
+    function inst (all){
+        const instList = document.createElement('li')
+        const instListText = document.createTextNode(all)
+        
+        instList.appendChild(instListText)
+        return instList
+    }
+
+    for(let i=0; i<ingArr.length; i++) {
+        ingParent.appendChild(ing(ingArr[i].ingredients))
+    }
+    for(let i=0; i<instArr.length; i++) {
+        instParent.appendChild(ing(instArr[i].instructions))
+    }
+    
+    foodImage.src = JSON.parse(recipes)[2].image
+    foodName.innerHTML = JSON.parse(recipes)[2].name
+}
+
+const yam = () =>{
+    const foodImage = document.getElementById('foodImage')
+    const foodName = document.getElementById('foodName')
+
+    const ingParent = document.getElementById('ingredientList')
+    const ingArr = JSON.parse(recipes)[1].ingredient
+    const instArr = JSON.parse(recipes)[1].instruction
+    const instParent = document.getElementById('instructionList')
+    const instList = document.createElement('li')
+
+    while(ingParent.hasChildNodes()){
+        ingParent.removeChild(ingParent.firstChild)
+    }
+    while(instParent.hasChildNodes()){
+        instParent.removeChild(instParent.firstChild)
+    }
+
+    function ing (all){
+        const ingList = document.createElement('li')
+        const ingListText = document.createTextNode(all)
+        
+        ingList.appendChild(ingListText)
+        return ingList
+    }
+    function inst (all){
+        const instList = document.createElement('li')
+        const instListText = document.createTextNode(all)
+        
+        instList.appendChild(instListText)
+        return instList
+    }
+
+    for(let i=0; i<ingArr.length; i++) {
+        ingParent.appendChild(ing(ingArr[i].ingredients))
+    }
+    for(let i=0; i<instArr.length; i++) {
+        instParent.appendChild(ing(instArr[i].instructions))
+    }
+
+    foodImage.src = JSON.parse(recipes)[1].image
+    foodName.innerHTML = JSON.parse(recipes)[1].name
+}
+
+const salad = () =>{
+    const foodImage = document.getElementById('foodImage')
+    const foodName = document.getElementById('foodName')
+
+    const ingParent = document.getElementById('ingredientList')
+    const ingArr = JSON.parse(recipes)[5].ingredient
+    const instArr = JSON.parse(recipes)[5].instruction
+    const instParent = document.getElementById('instructionList')
+    const instList = document.createElement('li')
+
+    while(ingParent.hasChildNodes()){
+        ingParent.removeChild(ingParent.firstChild)
+    }
+    while(instParent.hasChildNodes()){
+        instParent.removeChild(instParent.firstChild)
+    }
+
+    function ing (all){
+        const ingList = document.createElement('li')
+        const ingListText = document.createTextNode(all)
+        
+        ingList.appendChild(ingListText)
+        return ingList
+    }
+    function inst (all){
+        const instList = document.createElement('li')
+        const instListText = document.createTextNode(all)
+        
+        instList.appendChild(instListText)
+        return instList
+    }
+
+    for(let i=0; i<ingArr.length; i++) {
+        ingParent.appendChild(ing(ingArr[i].ingredients))
+    }
+    for(let i=0; i<instArr.length; i++) {
+        instParent.appendChild(ing(instArr[i].instructions))
+    }
+
+    foodImage.src = JSON.parse(recipes)[5].image
+    foodName.innerHTML = JSON.parse(recipes)[5].name
+}
+
+const iyan = () =>{
+    const foodImage = document.getElementById('foodImage')
+    const foodName = document.getElementById('foodName')
+
+    const ingParent = document.getElementById('ingredientList')
+    const ingArr = JSON.parse(recipes)[4].ingredient
+    const instArr = JSON.parse(recipes)[4].instruction
+    const instParent = document.getElementById('instructionList')
+    const instList = document.createElement('li')
+
+    while(ingParent.hasChildNodes()){
+        ingParent.removeChild(ingParent.firstChild)
+    }
+    while(instParent.hasChildNodes()){
+        instParent.removeChild(instParent.firstChild)
+    }
+
+    function ing (all){
+        const ingList = document.createElement('li')
+        const ingListText = document.createTextNode(all)
+        
+        ingList.appendChild(ingListText)
+        return ingList
+    }
+    function inst (all){
+        const instList = document.createElement('li')
+        const instListText = document.createTextNode(all)
+        
+        instList.appendChild(instListText)
+        return instList
+    }
+
+    for(let i=0; i<ingArr.length; i++) {
+        ingParent.appendChild(ing(ingArr[i].ingredients))
+    }
+    for(let i=0; i<instArr.length; i++) {
+        instParent.appendChild(ing(instArr[i].instructions))
+    }
+
+    foodImage.src = JSON.parse(recipes)[4].image
+    foodName.innerHTML = JSON.parse(recipes)[4].name
+}
